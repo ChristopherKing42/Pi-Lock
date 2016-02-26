b752a236a375f6b3cb6638597993f26047255447
Christopher King <G.nius.ck@gmail.com>
2016-02-26 13:01:19 -0500

    CheckAccess (Unfinished)

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import pyudev
+import pyudev, glob
 context = pyudev.Context()
 monitor = pyudev.Monitor.from_netlink(context)
 monitor.filter_by('block')
 dvItr = iter(monitor.poll, None)
 
+directory = "."
+
 def waitForUsb():
     for dev in dvItr:
         if dev.action == "add" and dev["DEVTYPE"] == "partition":
             yield dev.device_node
+
+def CheckAccess(usb):
+    for key in glob.glob(directory + "/*.key"):
+        for maykey in glob.glob(usb + "/*.key"):
+            print key,maykey
